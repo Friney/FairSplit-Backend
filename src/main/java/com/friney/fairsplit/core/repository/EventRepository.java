c0f8e141497dbd899908f64a20f6df2e10271e75
Friney <bso210403@gmail.com>
2025-02-03 20:38:44 +0700

    fix N+1 problem

--- src/main/java/com/friney/fairsplit/core/repository/EventRepository.java
+++ src/main/java/com/friney/fairsplit/core/repository/EventRepository.java
@@ -1,9 +1,22 @@
 package com.friney.fairsplit.core.repository;
 
 import com.friney.fairsplit.core.entity.Event.Event;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
+
+    @Override
+    @EntityGraph(value = "Event.withReceiptsAndExpenses", type = EntityGraph.EntityGraphType.LOAD)
+    List<Event> findAll();
+
+    @Override
+    @EntityGraph(value = "Event.withReceiptsAndExpenses", type = EntityGraph.EntityGraphType.LOAD)
+    Optional<Event> findById(Long id);
 }
