a1c4a378ccd040f60b3775c2f75d69ea91c4e58b
Friney <bso210403@gmail.com>
2025-05-06 17:38:11 +0700

    delete build

--- build/resources/main/db/migration/V3__add_password_for_registered_users.sql
+++
@@ -1,9 +0,0 @@
-alter table registered_users
-    add column password varchar(255);
-
-update registered_users
-set password = 'password'
-where password is null;
-
-alter table registered_users
-    alter column password set not null;
